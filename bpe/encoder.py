b5fa297fa24399d55d7c9a2900a5d3ac625a3b21
Stuart Axelbrooke <stuart@axelbrooke.com>
2017-09-22 17:32:42 +0900

    WIP commit moving machines

--- bpe/encoder.py
+++ bpe/encoder.py
@@ -1,13 +1,135 @@
 # coding=utf-8
 """ An encoder which learns byte pair encodings for white-space separated text.  Can tokenize, encode, and decode. """
+from collections import Counter
+
+from nltk.tokenize import casual_tokenize
+from tqdm import tqdm
+import toolz
+
+EOW = '</w>'
 
 
 class Encoder:
     """ Encodes white-space separated text using byte-pair encoding.  See https://arxiv.org/abs/1508.07909 for details.
     """
 
-    def __init__(self, vocab_size=32768):
-        if vocab_size < 1:
-            raise ValueError('vocab_size must be greater than 0.')
+    def __init__(self, vocab_size=32768, bpe_vocab_size=None, word_tokenizer=casual_tokenize, silent=False, ngram_min=2,
+                 ngram_max=4, batch_size=1000000):
+        if vocab_size < 1 and (bpe_vocab_size is not None and bpe_vocab_size < 0):
+            raise ValueError('vocab size must be greater than 0.')
 
         self.vocab_size = vocab_size
+        self.bpe_vocab_size = bpe_vocab_size or vocab_size
+        self.word_tokenizer = word_tokenizer
+        self.vocab = {}
+        self.inverted_vocab = {}
+        self._progress_bar = iter if silent else tqdm
+        self.ngram_min = ngram_min
+        self.ngram_max = ngram_max
+        self.batch_size = batch_size
+
+    def byte_pair_counts(self, text):
+        """ Counts space separated token character pairs:
+            [('T h i s </w>', 4}] -> {'Th': 4, 'hi': 4, 'is': 4, 's</w>': 4}
+        """
+        for token, count in self._progress_bar(self.count_tokens(text).items()):
+            bp_counts = Counter()
+            for ngram in token.split(' '):
+                bp_counts[ngram] += count
+            for ngram_size in range(self.ngram_min, min([self.ngram_max, len(token)])):
+                ngrams = [''.join(ngram) for ngram in toolz.sliding_window(ngram_size, token.split(' '))]
+
+                for ngram in ngrams:
+                    bp_counts[''.join(ngram)] += count
+
+            if EOW in bp_counts:
+                del bp_counts[EOW]
+            yield bp_counts
+
+    def count_tokens(self, text):
+        """ Count tokens into a BPE vocab """
+        token_counts = Counter(toolz.concat(map(self.word_tokenizer, self._progress_bar(text))))
+        return {' '.join(token) + ' ' + EOW: count for token, count in token_counts.items()}
+
+    def fit(self, text):
+        """ Learn vocab from text. """
+        vocab = Counter()
+        for batch in toolz.partition_all(self.batch_size, self.byte_pair_counts(text)):
+            for counter in batch:
+                vocab += counter
+
+            self.trim_vocab(10 * self.bpe_vocab_size, vocab)
+
+        self.trim_vocab(self.bpe_vocab_size, vocab)
+        self.vocab = {pair: idx for idx, pair in enumerate(sorted(vocab.keys()))}
+        self.inverted_vocab = {idx: pair for pair, idx in self.vocab.items()}
+
+    @staticmethod
+    def trim_vocab(n, vocab):
+        """  Deletes all pairs below 10 * vocab size to prevent memory problems """
+        pair_counts = sorted(vocab.items(), key=lambda p: -p[1])
+        pairs_to_trim = [pair for pair, count in pair_counts[n:]]
+        for pair in pairs_to_trim:
+            del vocab[pair]
+
+    def subword_tokenize(self, word):
+        word += EOW
+        end_idx = len(word)
+        sw_tokens = []
+        start_idx = 0
+
+        while start_idx < len(word):
+            subword = word[start_idx:end_idx]
+            if subword in self.vocab:
+                yield subword
+                start_idx = end_idx
+                end_idx = len(word)
+            elif len(subword) == 1:
+                yield subword
+                start_idx = end_idx
+                end_idx = len(word)
+            elif len(subword) == 1 + len(EOW) and subword.endswith(EOW):
+                yield subword
+                start_idx = end_idx
+                end_idx = len(word)
+            else:
+                end_idx -= 1
+
+        return sw_tokens
+
+    def tokenize(self, sentence):
+        """  """
+        word_tokens = self.word_tokenizer(sentence)
+
+        tokens = []
+        for word_token in word_tokens:
+            if word_token + EOW in self.vocab:
+                tokens.append(word_token + EOW)
+            else:
+                tokens.extend(self.subword_tokenize(word_token))
+
+        return tokens
+
+    def encode(self, sentences):
+        """ Turns space separated tokens into vocab idxs """
+        for sentence in sentences:
+            yield [self.vocab[token] for token in self.tokenize(sentence)]
+
+    @classmethod
+    def tokens_to_words(cls, tokens):
+        words = []
+        current_word = []
+
+        for token in tokens:
+            if token.endswith(EOW):
+                current_word.append(token[-len(EOW):])
+                words.append(''.join(current_word))
+            else:
+                current_word.append(token)
+
+        return words
+
+    def decode(self, idx_rows):
+        """ Turns vocab indexes into tokens """
+        for idxs in idx_rows:
+            yield self.tokens_to_words([self.vocab[idx] for idx in idxs])
--- bpe/test/encoder_test.py
+++ bpe/test/encoder_test.py
@@ -3,16 +3,32 @@
 from hypothesis import given
 import hypothesis.strategies as st
 
-from bpe.encoder import Encoder
+from bpe.encoder import Encoder, EOW
+
+# Generated with http://pythonpsum.com
+test_corpus = '''Object raspberrypi functools dict kwargs. Gevent raspberrypi functools. Dunder raspberrypi decorator dict lambda zip import pyramid.
+Kwargs raspberrypi diversity unit object gevent. Import integration decorator unit django yield functools twisted. Dunder integration decorator goat future. Python raspberrypi community pypy. Kwargs integration beautiful test reduce gil python closure. Gevent integration generator test kwargs raise itertools.
+Reduce integration coroutine bdfl python. Cython integration beautiful list python.
+Object raspberrypi diversity 2to3 dunder script. Python integration exception dict kwargs dunder pycon. Import raspberrypi beautiful test import six web. Future integration mercurial self script web. Return raspberrypi community test.
+Django raspberrypi mercurial unit import yield raspberrypi rocksdahouse. Dunder raspberrypi mercurial list reduce class test scipy.
+Object integration beautiful 2to3. Kwargs raspberrypi beautiful dict lambda class generator. Django integration coroutine dict import web map pyramid. Kwargs raspberrypi diversity dict future scipy raspberrypi rocksdahouse.
+Import raspberrypi exception list object. Object raspberrypi coroutine unit lambda sys django. Method raspberrypi generator test reduce future tuple. Kwargs raspberrypi decorator list cython def import twisted.
+Method raspberrypi beautiful unit method cython implicit zip. Dunder integration generator dict gevent def.
+Gevent integration decorator test python object guido. Reduce integration beautiful goat.
+Method raspberrypi diversity pypi return tuple list. Django integration functools. Method integration beautiful self return future kwargs. Gevent raspberrypi functools unit lambda zip python science functools. Future raspberrypi community pypy return six cython.
+'''.split('\n')
 
 
-def test_encoder_creation():
+
+@given(st.integers(min_value=1))
+def test_encoder_creation(vocab_size):
     """ Should be able to instantiate an Encoder with expected params """
-    Encoder(vocab_size=20)
+    Encoder(vocab_size=vocab_size)
 
 
 @given(st.integers(max_value=0))
 def test_encoder_creation_graceful_failure(vocab_size):
+    """ Min vocab size is 1.  Anything lower should ValueError """
     died = False
 
     try:
@@ -21,3 +37,29 @@ def test_encoder_creation_graceful_failure(vocab_size):
         died = True
 
     assert died, "Encoder should have raised a ValueError for < 1 vocab size"
+
+
+def test_encoder_fit():
+    """ Encoer should be able to fit to provided text data. """
+    encoder = Encoder(silent=True)
+    encoder.fit(test_corpus)
+    assert encoder.tokenize('from toolz import reduce') == ['f', 'ro', 'm' + EOW,
+                                                            'tool', 'z' + EOW,
+                                                            'import' + EOW,
+                                                            'reduce' + EOW]
+
+
+def test_single_letter_encoding():
+    """ Should yield single letters when untrained """
+    encoder = Encoder()
+    assert encoder.tokenize('single letters') == list('singl') + ['e' + EOW] + list('letter') + ['s' + EOW]
+
+
+def test_unseen_word_ending():
+    """ The last character should come with a </w> even if it wasn't seen as the last letter of a word in the training
+        set.
+    """
+    encoder = Encoder(silent=True)
+    encoder.fit(test_corpus)
+    assert encoder.tokenize('import toolz') == ['import' + EOW, 'tool', 'z' + EOW]
+
