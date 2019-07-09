8eecb195ee2f5ba3f95ce97a8c731a3838f71ffe
Pascal van Kooten <pascal@vks.ai>
2019-07-09 20:56:38 +0200

    use tok as fallback tokenizer

--- bpe/encoder.py
+++ bpe/encoder.py
@@ -7,7 +7,7 @@ try:
 except ImportError:
     pass
 
-from nltk.tokenize import wordpunct_tokenize
+from tok import word_tokenize
 from tqdm import tqdm
 import toolz
 import json
@@ -39,7 +39,7 @@ class Encoder:
         self.pct_bpe = pct_bpe
         self.word_vocab_size = max([int(vocab_size * (1 - pct_bpe)), len(self.required_tokens or [])])
         self.bpe_vocab_size = vocab_size - self.word_vocab_size
-        self.word_tokenizer = word_tokenizer if word_tokenizer is not None else wordpunct_tokenize
+        self.word_tokenizer = word_tokenizer if word_tokenizer is not None else word_tokenize
         self.custom_tokenizer = word_tokenizer is not None
         self.word_vocab = {}  # type: Dict[str, int]
         self.bpe_vocab = {}  # type: Dict[str, int]
